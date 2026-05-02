39843806829a06218d6026e46e29a2d7162c6937
DupeMaxSab77 <hokah261@gmail.com>
2026-05-02 10:24:50 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
+import os
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    # Busca automáticamente en la carpeta 'templates'
     return render_template('index.html')
 
 if __name__ == '__main__':
-    # host='0.0.0.0' permite acceso desde otros dispositivos en la red local
-    app.run(debug=True, port=5000)
+    # This line is the key: It gets the port from Railway
+    port = int(os.environ.get('PORT', 5000))
+    # host='0.0.0.0' is required for the internet to see your app
+    app.run(host='0.0.0.0', port=port)
